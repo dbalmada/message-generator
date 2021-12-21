3e4aaaef229bac7dbd371997e46beed62ec444ce
dbalmada <dbalmada@icloud.com>
2021-12-21 15:09:12 +0100

    Message
    MVP:
    * Random message generator: created the functionality.
    * Two buttons: generate message and clean the message box
    
    Future improvements:
    * Work with an API to fetch phrases to generate the message randomly.
    * Clean the message box each time the generate button is clicked and generate a new random message, instead of displaying the same one.
    * Bring improvements to the UI.
    * Provide the users with options like: subject, theme, gender, length, etc.

---
+++ index.js
@@ -0,0 +1,28 @@
+let createMessage = document.getElementById("generator-Btn");
+let messageBox = document.getElementById("message-box")
+
+
+
+let message = {
+    articles: ["The", "He", "She", "They"],
+    subjectOne: ["kapper", "gardener", "barista", "shopper"],
+    verbs: ["went", "jumped", "laughed", "talked"],
+    connector: ["to", "from", "on", "so"],
+    subjects: ["Leyla", "Sophie", "Max", "Rosa"],
+}
+
+ let randomNum = Math.floor(Math.random()*4);
+
+
+createMessage.addEventListener('click', function(){
+    if (messageBox.innerText != ""){
+        messageBox.innerText = "";
+    }
+    messageBox.innerText = message.articles[randomNum] + ' ' + message.subjectOne[randomNum] + ' ' + message.verbs[randomNum] + ' ' + message.connector[randomNum] + ' ' + message.subjects[randomNum]
+})
+
+document.getElementById('delete-Btn').addEventListener('click', function(){
+    messageBox.innerText = "";
+})
+
+
